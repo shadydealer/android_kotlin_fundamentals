68d8429051ab4c12eea00bb9038c43d76238e77b
Dimitar Lukanov <dimiturlukanov@gmail.com>
2021-06-24 17:47:39 +0300

    Made the dice roller app roll d6 instead of a d20 cause i dont have images for d20

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -9,7 +9,7 @@ import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     val start: Int = 1
-    val end: Int = 20
+    val end: Int = 6
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
