c93ed7e835978276cbb1a7aee71a99d4f6fe4276
Dimitar Lukanov <dimiturlukanov@gmail.com>
2021-06-28 14:19:25 +0300

    Added images of a d6 which are being used when rolling the dice or counting up

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,42 +1,70 @@
 package com.example.diceroller
 
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
-import org.w3c.dom.Text
+import android.widget.ImageView
+import androidx.core.content.res.ResourcesCompat
 
 class MainActivity : AppCompatActivity() {
-    val start: Int = 1
-    val end: Int = 6
+    private val start = 1
+    private val end: Int = 6
+    private val resourcePathPrefix: String = "drawable/dice_"
+
+    private lateinit var countUpButton: Button
+    private lateinit var rollButton: Button
+    private lateinit var imageView: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val rollButton: Button = findViewById(R.id.roll_button)
+
+        rollButton = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
 
-        val countUpButton: Button = findViewById(R.id.count_up_button)
+        countUpButton = findViewById(R.id.count_up_button)
         countUpButton.setOnClickListener { countUp() }
+
+        imageView = findViewById(R.id.dice_image)
     }
 
     private fun rollDice() {
-        val textView: TextView = findViewById(R.id.text_view)
         val randomNumber: Int = (start..end).random()
-        textView.text = randomNumber.toString()
+        setImageView(randomNumber)
     }
 
     private fun countUp() {
-        val textView: TextView = findViewById(R.id.text_view)
-        val currentValue: Int? = textView.text.toString().toIntOrNull()
-
-        val newValue: Int
+        val currentValue: String? = imageView.tag as String?
         if (currentValue != null) {
-            newValue = if (currentValue < end) currentValue + 1 else currentValue
+            val matchResults: MatchResult? = """$resourcePathPrefix(\d)"""
+                .toRegex()
+                .matchEntire(currentValue)
+            val side: Int = matchResults!!.groups[1]!!.value.toInt()
+            if (side < end) {
+                setImageView(side+1)
+            }
         }else {
-            newValue = start
+            setImageView(1)
         }
-        textView.text = newValue.toString()
+    }
+
+    private fun setImageView(number: Int) {
+        val resourcePath = "$resourcePathPrefix$number"
+        val imageResource: Int = resources.getIdentifier(
+            resourcePath,
+            null,
+            packageName
+        )
+        val drawableImage: Drawable? = ResourcesCompat.getDrawable(
+            resources,
+            imageResource,
+            null
+        )
+
+        if (drawableImage != null) {
+            imageView.setImageDrawable(drawableImage)
+            imageView.tag = "$resourcePathPrefix$number"
+        }
     }
 }
