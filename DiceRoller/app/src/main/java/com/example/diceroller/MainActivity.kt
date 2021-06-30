4d07ba11d2a97b3fa390adb0bb999ad7cc65d1d5
Dimitar Lukanov <dimiturlukanov@gmail.com>
2021-06-30 13:17:07 +0300

    Made it so the users input influences how many dice he sees on the screen

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.diceroller
 
+import android.content.Context
 import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
+import android.view.Gravity
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.*
 import androidx.core.content.res.ResourcesCompat
 
 class MainActivity : AppCompatActivity() {
@@ -13,26 +17,44 @@ class MainActivity : AppCompatActivity() {
     private val resourcePathPrefix: String = "drawable/dice_"
 
     private lateinit var rollButton: Button
+    private lateinit var diceImageLinearLayout: LinearLayout
     private lateinit var diceImages: Array<ImageView>
+    private lateinit var diceToRollCountInput: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         rollButton = findViewById(R.id.roll_button)
-        rollButton.setOnClickListener { rollDice() }
+        rollButton.setOnClickListener { rollDice(it) }
 
-        diceImages = arrayOf(
-            findViewById(R.id.dice_image_1),
-            findViewById(R.id.dice_image_2)
-        )
+        diceImageLinearLayout = findViewById(R.id.dice_images_layout)
+
+        diceToRollCountInput = findViewById(R.id.dice_to_roll_count)
+        diceToRollCountInput.setOnEditorActionListener {v, actionId, _ ->
+            if(actionId == EditorInfo.IME_ACTION_DONE) {
+                rollDice(v)
+                true
+            } else {
+                false
+            }
+        }
     }
 
-    private fun rollDice() {
-        for(diceImage in diceImages) {
+    private fun rollDice(view: View) {
+        diceImageLinearLayout.removeAllViews()
+        generateDiceImages()
+        for (diceImage in diceImages) {
             val randomNumber: Int = (start..end).random()
             setImageView(diceImage, randomNumber)
         }
+        hideKeyboard(view)
+    }
+
+    private fun hideKeyboard(view: View) {
+        val inputMethodManager =
+            getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
     }
 
     private fun setImageView(diceImage: ImageView, number: Int) {
@@ -51,5 +73,32 @@ class MainActivity : AppCompatActivity() {
         if (drawableImage != null) {
             diceImage.setImageDrawable(drawableImage)
         }
+    }
+
+    private fun generateDiceImages() {
+        val count: Int = diceToRollCountInput.text.toString().toIntOrNull() ?: 0
+        diceImages = Array(count) {
+            val imageView: ImageView = buildDiceImage()
+            diceImageLinearLayout.addView(imageView)
+            imageView
+        }
+    }
+
+    private fun buildDiceImage(): ImageView {
+        val imageView = ImageView(this)
+        imageView.layoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.WRAP_CONTENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        ).apply {
+            gravity = Gravity.CENTER_HORIZONTAL
+        }
+        imageView.setImageDrawable(
+            ResourcesCompat.getDrawable(
+                resources,
+                R.drawable.empty_dice,
+                null
+            )
+        )
+        return imageView
     }
 }
